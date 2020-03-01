7c1daecc03d0062d3737101a402d2e84b4450f56
tangratu <47471719+tangratu@users.noreply.github.com>
2020-03-01 10:13:21 +0200

    Just a test.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Dan";
+            int num = 1;
+            MySqlConnection usercon = new MySqlConnection(
+                "Server=localhost;" +
+                "Database=usersdb;" +
+                "User ID=root;" +
+                "Password=Tangra;" +
+                "Integrated Security=false;");
+            usercon.Open();
+            using (usercon)
+            {
+                MySqlCommand Add = new MySqlCommand("INSERT INTO profiles (username) " +
+                    "VALUES ('" + name + "')", usercon);
+                MySqlCommand Delete = new MySqlCommand("DELETE FROM profiles WHERE (id = '" + num + "')", usercon);
+                MySqlCommand Update_lessons = new MySqlCommand("UPDATE profiles SET lessons = lessons + 1 WHERE id = '" + num + "' ", usercon);
+                MySqlCommand Get_lessons = new MySqlCommand("SELECT lessons FRom profiles p WHERE p.id ='" + num + "'", usercon);
+                Update_lessons.ExecuteNonQuery();
+                usercon.Close();
+
+            }
+        }
+    }
+}
